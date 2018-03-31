e80cf8b8606b5cbe401db3d21310fe0972a13e2a
= <nncl@live.com>
2018-03-31 20:40:55 -0300

    Modules

---
+++ script.ts
@@ -0,0 +1,6 @@
+// import {square, hello} from "./utils";
+// import * as utils from "./utils";
+import square, {hello} from "./utils";
+
+console.log(square(2));
+hello();
---
+++ utils.ts
@@ -0,0 +1,13 @@
+// function square(x) {
+//     return Math.pow(x, 2);
+// }
+
+export default function square(x) {
+    return Math.pow(x, 2);
+}
+
+export function hello() {
+    console.log("Hello World");
+}
+
+// export {square, hello};
