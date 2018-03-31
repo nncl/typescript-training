bb3cb659179acefd7eb1207e10f8b27f036825cf
= <nncl@live.com>
2018-03-31 17:07:06 -0300

    Destructuring values from variables

--- hello.ts
+++ hello.ts
@@ -1,2 +1,17 @@
 console.log(`Hello
 World :) Bye bye`);
+
+// Destructuring o/
+const obj = {first: "Caue", last: "Almeida"};
+const {first: f} = obj;
+console.log(f); // damn
+
+const {first} = obj;
+console.log(first); // shortcode
+
+// Declaring default x value
+function fn({x = 0}){
+    console.log(x);
+}
+
+fn({x: 1});
